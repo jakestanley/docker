7f3e437745f9dc0f459ca75f9899672124c7fcfb
Jake Stanley <prudentia713@gmail.com>
2024-02-05 21:17:05 +0000

    rust script

---
+++ rust.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+CONFIG_DIR=$HOME/.config/com.github.jakestanley.docker
+mkdir -p $CONFIG_DIR
+
+ENV_FILE=$CONFIG_DIR/rust.env
+touch $ENV_FILE
+
+prompt() {
+    read -s -p "Enter the desired RCON password for the server" rcon_password
+    echo "RUST_RCON_PASSWORD=$rcon_password" >> $ENV_FILE
+}
+
+if grep -q "^RUST_RCON_PASSWORD=" "$ENV_FILE"; then
+    echo "Password is set"
+else
+    prompt
+fi
+    
+docker run --name rust-server \
+	-p 28015:28015 \
+	-p 28015:28015/udp \
+	-p 8082:8080 \
+	-e RUST_SERVER_NAME=Stanners \
+	-e RUST_SERVER_WORLDSIZE=3500 \
+	--env-file $ENV_FILE \
+	-v $HOME/rust:/steamcmd/rust \
+	didstopia/rust-server 
