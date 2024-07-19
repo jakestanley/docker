7912c538d08ab853c36788bfe4557e1cbc04fbd1
Jake Stanley <prudentia713@gmail.com>
2024-07-19 10:44:07 +0000

    set homepage config dir

--- arch_x86/qbittorrent/start.sh
+++ arch_x86/qbittorrent/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p ~/homepage/config
 mkdir -p ~/syncthing/config
-PID=$(id -u) GID=$(id -g) docker compose up -d
+HOMEPAGE_CONFIG_DIR="$HOME/homepage/config" PID=$(id -u) GID=$(id -g) docker compose up -d
